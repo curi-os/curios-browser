66e2768fb24a5e7fc596b16c0fe052269e5a9e14
Alexandre Magno <alexanmtz@gmail.com>
2026-03-12 14:49:37 +0100

    fixing logo

--- src/components/chat/SidebarHeader/SidebarHeader.tsx
+++ src/components/chat/SidebarHeader/SidebarHeader.tsx
@@ -30,7 +30,7 @@ export default function SidebarHeader({
         <div className="flex items-center gap-2">
           <div>
             <img
-              className={`h-10 w-10 rounded-2xl ${logoBgClass}`}
+              className={`w-10 rounded-2xl ${logoBgClass}`}
               src={curiosLogoWhiteUrl}
               alt={`${APP_NAME} logo`}
             />
